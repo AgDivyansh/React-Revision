5a4a27eec584aa611eb2ee3aaa36b9efa6a61bc8
AgDivyansh <93419706+AgDivyansh@users.noreply.github.com>
2024-08-01 01:17:31 +0530

    bg changer using react+vite

---
+++ 04_BG-changer/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState } from 'react'
+const [color, setColor] = useState(0) ;
+function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
