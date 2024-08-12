42cdf40862fc39ce2a2beb131d64629d51efadd2
AgDivyansh <93419706+AgDivyansh@users.noreply.github.com>
2024-08-13 01:05:25 +0530

    reating context provider to create context apid

---
+++ Context-API/Theme-Toggler/src/Context/UserContextProvider.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import UserContext from "./Usercontext";
+
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null) ;
+  return(
+    
+  )
+}
+
+export default UserContextProvider ;
